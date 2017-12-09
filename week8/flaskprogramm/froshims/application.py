74608f26ee7fa7ccf6cba8bd437ca4e66df8ff45
rin <naken505@gmail.com>
2017-12-09 22:43:10 +0300

    snooze

---
+++ week8/flaskprogramm/froshims/application.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, render_template, request, sessions, url_for
+import csv
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.form["name"] == "" or request.form["dorm"] == "":
+        return render_template("failure.html")
+    file = open("registrants.csv", "a")
+    writer = csv.writer(file)
+    writer.writerow((request.form["name"], request.form["dorm"]))
+    return render_template('success.html')
